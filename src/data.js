46bba7a962cf2dee2273b14d8865b0dc04b03c1b
Alexandre Claveau <a.claveau29@gmail.com>
2020-11-06 16:30:30 +0100

    Add data in static method

---
+++ src/data.js
@@ -0,0 +1,79 @@
+const data = {
+  perso: {
+    prenom: "Alexandre",
+    nom: "Claveau",
+    job: "Développeur Freelance",
+    phrase: "Ingénieur passioné par l'innovation et la programmation",
+  },
+  experiences: [
+    {
+      id: 1,
+      annees: "2019-2020",
+      job: "Développeur Front-end",
+      entreprise: "Olympp",
+      typeContrat: "CDI",
+      lieu: "Levallois-Perret (92)",
+      client: "L'Oréal",
+      duree: "9 mois",
+      explications:
+        "J'ai intégré une équipe au sein de la DSIRH de L’Oréal afin de développer un outil interne. Celui-ci permet de gérer les différentes formations proposées aux collaborateurs de chez L’Oréal.",
+    },
+    {
+      id: 2,
+      annees: "2018-2019",
+      job: "Alternant Développeur Front-end",
+      entreprise: "IT-CE",
+      typeContrat: "Contrat alternance",
+      lieu: "Rennes (35)",
+      client: null,
+      duree: "1 an",
+      explications:
+        "Maintenance et évolutions sur un outil dédié aux professionnels du groupe BPCE. De plus j'ai pu également développer un outil de démonstration afin de promouvoir l'étendu de CE net aux différents clients du groupe. Enfin, sur la fin de mon alternance, j'ai eu l'opportunité d'étendre mes compétences sur la mise en place d'intégration continue au sein de mon équipe.",
+    },
+    {
+      id: 3,
+      annees: "2016-2017",
+      job: "Alternant Développeur Front-end",
+      entreprise: "Crédit Mutuel ARKEA",
+      typeContrat: "Contrat alternance",
+      lieu: "Brest (29)",
+      client: null,
+      duree: "1 an",
+      explications:
+        "J’ai réalisé mon alternance au sein d’une nouvelle feature team et dans un cadre Agile/Scrum. On réalisait le développement de solutions innovantes à usage interne et à destination des clients finaux du Crédit Mutuel ARKEA. Nous avons développé des outils de monitoring, des outils utilisés par des téléopérateurs, une solution de génération automatique de tests et des nouvelles fonctionnalités à destination des clients finaux. En complément de mon rôle de développeur, j’occupe le rôle de Scrum Master en alternance avec d’autres membres de l’équipe.",
+    },
+    {
+      id: 4,
+      annees: "2016",
+      job: "Stage Développeur Service Banque à Distance",
+      entreprise: "Crédit Mutuel ARKEA",
+      typeContrat: "Stage Technicien",
+      lieu: "Brest (29)",
+      client: null,
+      duree: "7 semaines",
+      explications:
+        "Durant ce stage, j’ai travaillé sur une application web de gestion des formulaires remplis par des clients ou des prospects. Ces formulaires sont des demandes d’informations commerciales ou des demandes diverses (exemple : questions, demandes de devis ou de souscriptions).",
+    },
+  ],
+  competences: {
+    web: {
+      skills: "HTML, CSS, JavaScript",
+      tools:
+        "React.js, Bootstrap, Node.js, MongoDB, MySQL, lodash, JQuery, Git",
+    },
+    design: {
+      skills: "Web, Logos",
+      tools: "Figma, Affinity Designer, Illustrator, Photoshop",
+    },
+    management: {
+      skills: "Agile SCRUM",
+      tools: "JIRA",
+    },
+  },
+  social: {
+    linkedin: "https://www.linkedin.com/in/alexandre-claveau/",
+    github: "https://github.com/ClaveauAlex",
+    instagram: "https://www.instagram.com/alexandre_claveau.me/",
+    email: "test.test@toto.com",
+  },
+};
