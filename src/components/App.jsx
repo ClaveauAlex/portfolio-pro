31bd3c623c8891dbab91e207023af8a1e3a5eeaa
Alexandre Claveau <a.claveau29@gmail.com>
2020-11-09 17:19:55 +0100

    Dev header section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 
-function App(){
-    return (
+function App() {
+  return (
+    <section className="colored-section">
+      <div className="container-header">
         <nav>
-            <div className="navBarLeft">
-                <img src={process.env.PUBLIC_URL + '/logo.svg'} alt="logo"></img>
-            </div>
-            <div className="navBarRight">
-                <ul>
-                    <li>Projets</li>
-                    <li>Services</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-            <h1>Test Hello World !</h1>
+          <div className="navBarLeft">
+            <a href="/">
+              <img
+                src={process.env.PUBLIC_URL + "/logo_white.svg"}
+                className="logo"
+                alt="logo"
+              />
+            </a>
+          </div>
+          <div className="navBarRight">
+            <ul>
+              <li>
+                <a href="/home">Accueil</a>
+              </li>
+              <li>
+                <a href="/projets">Projets</a>
+              </li>
+              <li>
+                <a href="/services">Services</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <h1>Test Hello World !</h1>
         </nav>
-    );
+      </div>
+    </section>
+  );
 }
 
 export default App;
