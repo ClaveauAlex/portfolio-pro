5fbb02a68accc1af022ff266109b82d18f1d616a
Alexandre Claveau <a.claveau29@gmail.com>
2020-11-09 15:58:20 +0100

    Structure styles.css & add favicon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 function App(){
     return (
-        <div>
+        <nav>
+            <div className="navBarLeft">
+                <img src={process.env.PUBLIC_URL + '/logo.svg'} alt="logo"></img>
+            </div>
+            <div className="navBarRight">
+                <ul>
+                    <li>Projets</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
             <h1>Test Hello World !</h1>
-        </div>
+        </nav>
     );
 }
 
