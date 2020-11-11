0989c909ebcd8de5c2759fe205e557fca8d52def
Alexandre Claveau <a.claveau29@gmail.com>
2020-11-11 22:13:23 +0100

    Refactoring sur les routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import NavBar from "./NavBar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Error404 from "./pages/Error404";
 
 function App() {
   return (
     <section className="colored-section">
-      <div className="container-header">
-        <NavBar />
+      <div className="container-fluid">
+        <BrowserRouter>
+            <NavBar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/projects" component={Projects} />
+              <Route path="/services" component={Services} />
+              <Route path="/contact-me" component={Contact} />
+              <Route path="*" component={Error404} />
+            </Switch>
+        </BrowserRouter>
       </div>
     </section>
   );
