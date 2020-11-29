0fa0499c2f0fd2f62e02e3ca316c043d0cbc81cb
Alexandre Claveau <a.claveau29@gmail.com>
2020-11-29 15:05:23 +0100

    Reset project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import "./App.scss";
-import { BrowserRouter as Router,Route, Switch } from "react-router-dom";
-import Home from "./pages";
-// import SigninPage from "./pages/signin";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        {/* <Route path="/signin" component={SigninPage} exact />
-        <Route path="*" component={Error404} exact /> */}
-      </Switch>
-    </Router>
+    <div>
+      <h1>Portfolio</h1>
+    </div>
   );
 }
 
